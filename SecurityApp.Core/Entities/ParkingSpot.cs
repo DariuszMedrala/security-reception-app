90c61466c4e57e446dbf23fb3a82a6c290ef7b3a
Dariusz Mędrala <dariusz.medrala@student.pk.edu.pl>
2026-02-06 09:08:37 +0100

    feat(core): add ParkingSpot entity

---
+++ SecurityApp.Core/Entities/ParkingSpot.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SecurityApp.Core.Entities
+{
+    public class ParkingSpot
+    {
+        public int Id { get; set; }
+        [Required] public string SpotNumber { get; set; } = string.Empty;
+        public string? CurrentPlateNumber { get; set; }
+        public string CompanyName { get; set; } = string.Empty;
+        public DateTime PermissionStart { get; set; }
+        public DateTime PermissionEnd { get; set; }
+        public int BuildingId { get; set; }
+        public Building? Building { get; set; }
+    }
+}
