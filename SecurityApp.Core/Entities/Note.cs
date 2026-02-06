7908787970bb17b479df6dccbc60bdb3e6a057e8
Dariusz Mędrala <dariusz.medrala@student.pk.edu.pl>
2026-02-06 09:11:28 +0100

    feat(core): add Note entity

---
+++ SecurityApp.Core/Entities/Note.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SecurityApp.Core.Entities
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        [Required] public string Content { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime validFrom { get; set; }
+        public DateTime validUntil { get; set; }
+        public int BuildingId { get; set; }
+        public Building? Building { get; set; }
+    }
+}
