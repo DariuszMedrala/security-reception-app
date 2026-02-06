277a75e3507f8b0eadfbd6b6bf49d778ef1e7c89
Dariusz Mędrala <dariusz.medrala@student.pk.edu.pl>
2026-02-06 09:05:30 +0100

    feat(core): add PhonebookEntry entity

---
+++ SecurityApp.Core/Entities/PhonebookEntry.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SecurityApp.Core.Entities
+{
+    public class PhonebookEntry
+    {
+        public int Id { get; set; }
+        [Required] public string FirstName { get; set; } = string.Empty;
+        [Required] public string LastName { get; set; } = string.Empty;
+        [Required] public string PhoneNumber { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+        public int? BuildingId { get; set; }
+        public Building? Building { get; set; }
+    }
+}
